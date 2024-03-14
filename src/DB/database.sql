ee5b6011eb6c6f0b5956f7171188dac9ccb4635b
Alan09080 <alanortiz8097@gmail.com>
2024-03-14 08:32:24 -0600

    Update database.sql

--- src/DB/database.sql
+++ src/DB/database.sql
@@ -9,13 +9,18 @@ CREATE TABLE tableros (
     url VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
 
+-- Crear la tabla de usuarios
 CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     correo VARCHAR(255) UNIQUE NOT NULL,
-	usuario VARCHAR(255) NOT NULL,
-	contraseña VARCHAR(255) NOT NULL,
     id_tablero INT,
     FOREIGN KEY (id_tablero) REFERENCES tableros(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE auth (
+	id INT PRIMARY KEY,
+	usuario VARCHAR(20) NOT NULL,
+    pass VARCHAR(20) NOT NULL
 ) ENGINE=InnoDB;
 
 
